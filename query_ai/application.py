360dd7d0dbb0fea607b63dd80d44a4aff96ced07
Ron Webb <ron@ronella.xyz>
2025-02-19 23:37:07 +1300

    Externalize the logging configuration.

--- query_ai/application.py
+++ query_ai/application.py
@@ -1,7 +1,14 @@
 from query_ai.api import endpoints
 from flask import Flask
 
+from query_ai.logger import get_logger
+
 if __name__ == '__main__':
+
+    logger = get_logger(__name__)
+
+    logger.info("Query AI application")
+
     app = Flask(__name__)
 
     [endpoint(app) for endpoint in endpoints]
--- query_ai/logger/__init__.py
+++ query_ai/logger/__init__.py
@@ -1,26 +1,24 @@
 import os
 import logging
-from logging.handlers import RotatingFileHandler
+import logging.config
 
-__logs_directory = "../logs"  # Or whatever you want to call it
+__logs_directory = "../logs"
+__conf_directory = "../conf"
+
 if not os.path.exists(__logs_directory):
     os.makedirs(__logs_directory)
 
-__log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+__logger_ini_file = os.path.join(__conf_directory, 'logging.ini')
 
-__rotating_handler = RotatingFileHandler(os.path.join(__logs_directory, 'query-ai.log'),
-                                         maxBytes=1024*1024*10, # 10MB max size
-                                         backupCount=50)
-__rotating_handler.setFormatter(__log_formatter)
-
-__console_handler = logging.StreamHandler()  # Defaults to sys.stdout
-__console_handler.setFormatter(__log_formatter)
+if os.path.exists(__logger_ini_file):
+    logging.config.fileConfig(__logger_ini_file)
+else:
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('query_ai')
+    logger.warning("Config file not found, using basic config.")
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(__rotating_handler)
-    logger.addHandler(__console_handler)
     return logger
 
 __all__ = ['get_logger']
