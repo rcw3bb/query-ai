ef5468745e50884188883eb313b42a6e2d354585
Ron Webb <ron@ronella.xyz>
2025-02-20 00:45:15 +1300

    Lower the token length of the generated token.

--- query_ai/config/model_config.py
+++ query_ai/config/model_config.py
@@ -1,6 +1,6 @@
 class EmbeddingConfig:
     model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    token_length = 384
+    token_length = 300
 
 class GeneratorConfig:
     model_name = "google/flan-t5-large"
